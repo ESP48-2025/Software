21a4f438db6048ab4dce4646fa7022fb63f7c6c0
CuriosityDemon <yang.cheng-2@student.manchester.ac.uk>
2026-02-10 14:42:44 +0000

    sync2

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mbed.h"
+
+class Motor{
+    private:
+        PwmOut pwm, dir;
+        DigitalOut mode, enable;
+    public:
+        Motor(PinName p, PinName d, PinName m, PinName e):
+        pwm(p), dir(d), mode(m), enable(e){
+            enable = 0;
+            
+        }
+};
+//does this upload
