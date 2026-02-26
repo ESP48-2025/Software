202ec18042aaffa122dd1a81b3dc4bc8e8a3ea82
CuriosityDemon <yang.cheng-2@student.manchester.ac.uk>
2026-02-26 17:08:37 +0000

    autocalibration beta

--- esp_buggy.cpp
+++ esp_buggy.cpp
@@ -1,3 +1,5 @@
+// First revision, no testing yet. Completely beta.
+// Created on top of TD1 encoder code
 #include "mbed.h"
 #include "C12832.h"
 
@@ -59,6 +61,46 @@ Motor motorRight(D2, D3, PC_6);
 Potentiometer potLeft (A0, 3.3f);
 Potentiometer potRight(A1, 3.3f);
 
+#define CPR 511  
+
+class Encoder {
+public:
+    Encoder(PinName pinA, PinName pinB, float sam_f)
+        : A(pinA), B(pinB), sampling_freq(sam_f)
+    {
+        count = 0;
+        A.rise(callback(this, &Encoder::onA));
+        A.fall(callback(this, &Encoder::onA));
+        sampler.attach(callback(this, &Encoder::getCps), 1/sampling_freq);
+    }
+
+    void reset() { count = 0; }
+    int getCount() const { return count; }
+    float retrieveCps() const {return cps;}
+    float getRpm(int cpr) {return (cps/cpr*60);}
+    
+
+private:
+    InterruptIn A;
+    InterruptIn B;
+    volatile int count;
+    Ticker sampler;
+    float sampling_freq, cps;
+
+    void onA() {
+        if (A.read() == B.read()) count++;
+        else count--;
+    }
+    void getCps() {
+        cps = count*sampling_freq;
+        reset();
+    }
+};
+
+Encoder encLeft(PC_14, PC_15, 20);
+Encoder encRight(PB_8, PB_9, 20);
+
+
 int main(void) {
     // Enable both motors via single shared pin
     motorEnable.write(1);
@@ -72,27 +114,92 @@ int main(void) {
     motorRight.pwm_period(50);
     motorRight.is_forward(1);
 
+    float last_pot_L, last_pot_R;
+
+    encLeft.reset();
+    encRight.reset();
+
+
+    float cpsL = 0.0f;
+    float cpsR = 0.0f;
+
+    float pwmL, pwmR;
+    float changed_pwm, target_pwm;
+    float dutyCorrection = 0.1;
+    int correctionScale = 1;
+    // float m;        // m = L/R
+    float error = 1, error0 = 1, errorLast = 1;    // 1 - m
+    bool loop1 = 1;
+
+    float DL_DR_ratio[6];
+
     lcd.cls();
     lcd.locate(0, 0);
     lcd.printf("Buggy Ready");
     wait_ms(1000);
+    lcd.cls();
+    lcd.locate(0, 0);
+    lcd.printf("Matching duties:\n\nDL/DR:");
 
-    while(1) {
-        potLeft.sample();
-        potRight.sample();
+    for (int i = 5; i<11; i++){
+        target_pwm = float(i)/10;
+        // pwmL = target_pwm;
+        // pwmR = target_pwm;
+        // lcd.printf("Lduty: %.4f\tRduty: %.4f", pwmL, pwmR);
+        lcd.locate(((i-5)/6)*128, 8);
+        lcd.printf("%.2f", target_pwm);
+        
+        motorLeft.speed(target_pwm);
+        motorRight.speed(target_pwm);
 
-        float leftSpeed  = clamp(potLeft.getCurrentSampleNorm(),  0.0f, 1.0f);
-        float rightSpeed = clamp(potRight.getCurrentSampleNorm(), 0.0f, 1.0f);
+        // enter calibration loop
+        error = 1;
+        loop1 = 1;
+        while(abs(error) > 0.02){            
+            wait(3);
 
-        motorLeft.speed(leftSpeed);
-        motorRight.speed(rightSpeed);
+            // sample avearge cps
+            cpsL = 0;
+            cpsR = 0;
+            for (int j = 0; j < 10; j++){
+                wait_ms(500);
+                cpsL += encLeft.retrieveCps();
+                cpsR += encRight.retrieveCps();
+            }
+            // cpsL /= 10;
+            // cpsR /= 10;
 
-        lcd.cls();
-        lcd.locate(0, 0);
-        lcd.printf("L:%.2f R:%.2f", leftSpeed, rightSpeed);
-        lcd.locate(0, 16);
-        lcd.printf("Lv:%.1fV Rv:%.1fV", potLeft.getCurrentSampleVolts(), potRight.getCurrentSampleVolts());
+            // error decision
+            error = 1 - cpsL/cpsR;
+            if (loop1 == 1){
+                loop1 = 0;
+                error0 = error;
+                correctionScale = 1;
+            }
+            else if (errorLast*error < 0){
+                correctionScale++;
+            }
+            changed_pwm -= (error0*error)/abs(error0*error)*dutyCorrection/correctionScale;
+            if (error0 < 0){
+                pwmL = changed_pwm;
+                pwmR = target_pwm;
+            }
+            else{
+                pwmR = changed_pwm;
+                pwmL = target_pwm;
+            }
+            errorLast = error;
 
-        wait_ms(50);
+            // prep new speed
+            motorLeft.speed(pwmL);
+            motorRight.speed(pwmR);
+            
+        };
+        // error ~ 0, matched
+        DL_DR_ratio[i-5] = pwmL/pwmR;
+        lcd.locate(((i-5)/6)*128, 24);
+        lcd.printf("%.2f", DL_DR_ratio[i-5]);
+
     }
+    motorEnable.write(0);
 }
