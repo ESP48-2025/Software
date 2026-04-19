70cd24c9170fa20ac5e19f43939d8d20a69e344e
CuriosityDemon <yang.cheng-2@student.manchester.ac.uk>
2026-04-19 19:39:38 +0100

    Update Bluetooth.h

--- ESP48_Software_Library/Bluetooth.h
+++ ESP48_Software_Library/Bluetooth.h
@@ -3,9 +3,21 @@
 class Bluetooth {
 private:
     Serial hm10;
+    Ticker tick;
+    bool can_send;
+    void set_can_send(void){
+        can_send=1;
+    }
+    float clamp(float in, float max, float min){
+        if (in > max){return max;}
+        if (in < min){return min;}
+        return in;
+    }
 
 public:
-    Bluetooth() : hm10(PA_11, PA_12) {}
+    Bluetooth() : hm10(PA_11, PA_12) {
+        tick.attach(callback(this, &Bluetooth::set_can_send), 0.1);
+    }
 
     void begin() {
         hm10.baud(9600);
@@ -13,10 +25,26 @@ public:
 
     void sendWords(const char* text) {
         hm10.printf("%s\r\n", text);
+        can_send=0;
     }
 
     void sendSpeed(float rpmL, float rpmR) {
         hm10.printf("L=%.1f,R=%.1f\r\n", rpmL, rpmR);
+        can_send=0;
+    }
+
+    void sendTCRT(float LLL, float LL, float L, float R, float RR, float RRR){
+        LLL = clamp(LLL, 0.99, 0)*100;
+        LL = clamp(LL, 0.99, 0)*100;
+        L = clamp(L, 0.99, 0)*100;
+        R = clamp(R, 0.99, 0)*100;
+        RR = clamp(RR, 0.99, 0)*100;
+        RRR = clamp(RRR, 0.99, 0)*100;
+        hm10.printf(".%2d.%2d.%2d.%2d.%2d.%2d", LLL, LL, L, R, RR, RRR);
+    }
+
+    bool sendAvailable(void){
+        return can_send;
     }
 
     bool available() {
