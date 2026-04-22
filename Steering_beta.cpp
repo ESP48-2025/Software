f8a762571083dc7460c7af90daee3f25ee24a515
CuriosityDemon <yang.cheng-2@student.manchester.ac.uk>
2026-04-22 16:20:55 +0100

    Update Steering_beta.cpp
    PLEASE USE THE NEW PID CLASS

--- Steering_beta.cpp
+++ Steering_beta.cpp
@@ -1,10 +1,9 @@
 // Created: 14/04/2026
-// Last Updated: 21/04/2026
+// Last Updated: 22/04/2026
 // Main Author: Yang Cheng
 /*
-Added steering control on top of speed control.
-This file is created separately because LineFollowing.cpp is working code;
-whereas this one still requires testing
+patch notes:
+debugged speed control and steering, uses the 22/04/2026 PID class
 */
 
 #include "mbed.h"
@@ -56,7 +55,8 @@ int main(){
     
     // steering control
     float IRdata[6];
-    float tcrt_weighted, tcrt_response;
+    float tcrt_weighted;
+    float tcrt_response = 0.0f;
     float tcrt_total;
     PID Steering(50);
     Steering.setReference(3.5);
@@ -75,12 +75,12 @@ int main(){
         LeftMotor.setReference(refRpsL);
         LeftMotor.setGain(0.05, 0.3, 0);
         rpsL = 0 - encL.getRps();
-        pwmL = LeftMotor.updatePID(refRpsR, pwmL) + 0.513;     // offset at 0.513 duty
+        pwmL = LeftMotor.updatePID(refRpsL, pwmL) + 0.513;     // offset at 0.5 duty
         
-        RightMotor.setReference(-1.7);
+        RightMotor.setReference(refRpsR);
         RightMotor.setGain(0.045, 0.4, 0);
         rpsR = encR.getRps();
-        pwmR = RightMotor.updatePID(rpsR, pwmR) + 0.5;     // offset at 0.5 duty
+        pwmR = RightMotor.updatePID(refRpsR, pwmR) + 0.5;     // offset at 0.5 duty
         
         // measure wheel speed
         if (ble.sendAvailable()){
@@ -103,10 +103,17 @@ int main(){
             tcrt_weighted += (i+1)*IRdata[i];
             tcrt_total +=IRdata[i];
         }
-        tcrt_weighted = tcrt_weighted / tcrt_total;
+        if (tcrt_total > 1e-6){
+            tcrt_weighted = tcrt_weighted / tcrt_total;
+        }
+        else {
+            tcrt_weighted = 3.5f;
+            ble_report_cycle = 16;      // error message code 16: division by 0
+        }
 
         // tcrt PID beta -- questionable
-        tcrt_response = Steering.updatePID(tcrt_weighted, 0.5);
+        tcrt_response = Steering.updatePID(tcrt_weighted, tcrt_response/(2*refRps_Max)+0.5);    // second argument scaled to max at 0 and 1; min at 0.5
+        tcrt_response = clamp(tcrt_response, refRps_Max, -refRps_Max);
         if (tcrt_response > 0){      // Left drift -> turn right -> reduce right speed
             refRpsL = refRps_Max;
             refRpsR = refRps_Max - tcrt_response;
@@ -130,12 +137,15 @@ int main(){
                 ble.sendWords("Target RPS");
                 ble.sendSpeed(refRpsL, refRpsR);
                 break;
+            case 16:    // error message, do not loop
+                ble.sendWords("Steering: tcrt 0 div");
+                break;
             default:
                 ble.sendWords("nothing to report");
                 break;
         }
         ble_report_cycle += 1;
-        ble_report_cycle %= 2;
+        ble_report_cycle %= 2;  // skips error messages on loops
 
         wait(loop_time_s);
     }
